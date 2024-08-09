6074e51b01ed84423519939ba29c98a7a49bcbf3
Leonardo Ruma <leonardo.ruma@gmail.com>
2024-08-09 14:18:16 -0300

    adicionando globalProperties

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,40 +1,11 @@
-// import {App, Plugin} from 'vue';
-// import * as components from './index';
-//
-// interface SearchFrameworkOptions {
-//     projectName: string;
-//     prefixLabelI18n: string;
-// }
-//
-// const install: Plugin = (app: App, options: SearchFrameworkOptions) => {
-//     if (!options) {
-//         app.config.globalProperties.$searchFrameworkOptions = {
-//             projectName: 'projectName',
-//             prefixLabelI18n: 'label',
-//         };
-//     } else {
-//         app.config.globalProperties.$searchFrameworkOptions = options;
-//     }
-//     Object.entries(components).forEach(([componentName, component]) => {
-//         app.component(componentName, component);
-//     });
-// };
-//
-// export default install;
-//
-// export * from './index';
-
-
 import {App, Plugin} from 'vue';
 import * as components from './index';
 
-// Definição das opções do plugin
 interface SearchFrameworkOptions {
     projectName: string;
     prefixLabelI18n: string;
 }
 
-// Função de instalação executada por Vue.use()
 const install: Plugin<SearchFrameworkOptions> = (app: App, options?: SearchFrameworkOptions) => {
     if (!options) {
         app.config.globalProperties.$searchFrameworkOptions = {
@@ -49,12 +20,10 @@ const install: Plugin<SearchFrameworkOptions> = (app: App, options?: SearchFrame
     });
 };
 
-// Definindo o plugin explicitamente como um objeto com a função install
 const SearchFrameworkPlugin: Plugin<SearchFrameworkOptions> = {
     install
 };
 
-// Anexar exportações nomeadas diretamente ao plugin
 type NamedExports = Exclude<typeof components, 'default'>;
 type ExtendedPlugin = typeof SearchFrameworkPlugin & NamedExports;
 Object.entries(components).forEach(([componentName, component]) => {
